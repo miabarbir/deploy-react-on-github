76195f91f012ea3d12c7ada70408492244b707af
Mia Barbir <miabarbir13@gmail.com>
2019-12-11 22:00:58 +0100

    Updates

---
+++ precache-manifest.0091cd49b709e3e068fa901f69430739.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0979d9c44959ed421509306513afbc",
+    "url": "/deploy-react-on-github/index.html"
+  },
+  {
+    "revision": "306b0303f8c79efc7e24",
+    "url": "/deploy-react-on-github/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "127bdc670d414ad1d053",
+    "url": "/deploy-react-on-github/static/js/2.150129b1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy-react-on-github/static/js/2.150129b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "306b0303f8c79efc7e24",
+    "url": "/deploy-react-on-github/static/js/main.00f7fbab.chunk.js"
+  },
+  {
+    "revision": "60d448e617548a94fcfa",
+    "url": "/deploy-react-on-github/static/js/runtime-main.9cd84554.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react-on-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
